d219e5cf072f8086021fb567b9e38bbc986e50d8
PooyaAbbasi <poy.8383.abs@gmail.com>
2025-07-21 16:21:44 +0330

    refactor FullTextSearch components and separate some services in it.

---
+++ Phase03/Program.cs
@@ -0,0 +1,21 @@
+﻿using FullTextSearch.Services.FileReaderService;
+using FullTextSearch.Services.LoggerService;
+using FullTextSearch.Services.TokenizerService;
+
+namespace FullTextSearch;
+
+class Program
+{
+        static void Main(string[] args)
+        {
+                var logger = new ConsoleLogger();
+                var tokenizer = new Tokenizer();
+                var fileReader = new FileReader(logger);
+                var invertedIndex = new InvertedIndex.InvertedIndex(tokenizer);
+                
+                var app = new SearchApplication(fileReader, invertedIndex, logger);
+                app.Run();
+        }
+
+
+}
