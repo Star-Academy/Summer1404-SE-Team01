cb5542893d9173dcd34fc688208ee4b7447816f0
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2025-08-09 00:56:06 +0330

    Phase06 with CI/CD pipeline created.

---
+++ Phase06/FullTextSearch.Tests/TokenzerUnitTests.cs
@@ -0,0 +1,69 @@
+using FluentAssertions;
+using FullTextSearch.Services.TokenizerService;
+
+namespace FullTextSearch.Tests;
+public class TokenizerUnitTests
+{
+    private readonly Tokenizer _sut;
+
+    public TokenizerUnitTests()
+    {
+        _sut = new Tokenizer();
+    }
+
+    [Fact]
+    public void Tokenize_ShouldReturnUppercasedTokens_FromInputQuery()
+    {
+        var result = _sut.Tokenize("GET -hELp +illNeSS");
+
+        result.Should()
+            .BeEquivalentTo(new List<string> { "GET", "HELP", "ILLNESS" });
+    }
+
+    [Fact]
+    public void Tokenize_ShouldIgnorePunctuation()
+    {
+        var result = _sut.Tokenize("hello, world!");
+
+        result.Should()
+            .BeEquivalentTo(new List<string> { "HELLO", "WORLD" });
+    }
+
+    [Fact]
+    public void Tokenize_ShouldHandleApostrophes()
+    {
+        var result = _sut.Tokenize("it's raining");
+
+        result.Should()
+            .BeEquivalentTo(new List<string> { "IT", "S", "RAINING" });
+    }
+
+    [Theory]
+    [InlineData(" ")]
+    [InlineData("    ")]
+    public void Tokenize_ShouldReturnEmpty_ForEmptyString(string queryInput)
+    {
+        var result = _sut.Tokenize(queryInput);
+
+        result.Should()
+            .BeEmpty();
+    }
+
+    [Fact]
+    public void Tokenize_ShouldHandleMixedContent()
+    {
+        var result = _sut.Tokenize("Hello123 _test_!");
+
+        result.Should()
+            .BeEquivalentTo(new List<string> { "HELLO123", "_TEST_" });
+    }
+
+    [Fact]
+    public void Tokenize_ShouldNotReturnInvalidTokens()
+    {
+        var result = _sut.Tokenize("   ,,,   !");
+
+        result.Should()
+            .BeEmpty();
+    }
+}
---
+++ Phase06/FullTextSearch/InvertedIndex/SearchFeatures/AdvancedSearch.cs
@@ -0,0 +1,22 @@
+﻿using FullTextSearch.InvertedIndex.Dtos;
+using FullTextSearch.InvertedIndex.FilterStrategies.Abstractions;
+using FullTextSearch.InvertedIndex.SearchFeatures.Abstractions;
+
+namespace FullTextSearch.InvertedIndex.SearchFeatures;
+
+public class AdvancedSearch : IAdvancedSearch
+{
+
+    public HashSet<string> Search(QueryDto queryDto, InvertedIndexDto invIdxDto, IEnumerable<IFilterStrategy> filterStrategies)
+    {
+        var result = new HashSet<string>(invIdxDto.AllDocuments);
+
+        foreach (var strategy in filterStrategies)
+        {
+            var currDocIds = strategy.FilterDocumentsByQuery(queryDto, invIdxDto);
+            result.IntersectWith(currDocIds);
+        }
+
+        return result;
+    }
+}
