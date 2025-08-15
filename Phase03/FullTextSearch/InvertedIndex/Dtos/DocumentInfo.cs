5787aacdc20c0d4606c376b4a2bad8ec0b041741
PooyaAbbasi <poy.8383.abs@gmail.com>
2025-08-15 21:02:10 +0330

    Phase03 : Refactore FullTextSearch and clean architecture and codes structure.  (#3)
    * feat(full_text_search): add all components and test manually.
    
    * refactor FullTextSearch components and separate some services in it.
    
    * Phase03 project completed.
    
    * feat(full_text_search): add all components and test manually.
    
    * refactor FullTextSearch components and separate some services in it.
    
    * Phase03 project completed.
    
    * EnglishData files removed from remote
    
    * Advance search specification is made decoupled.
    
    * Advance search and filter specifications decoupled.
    
    * Folder structure fixed
    
    * Few modifications added
    
    * Few modifications added
    
    * Few changes added.
    
    * Few changes added.
    
    * Code structure refactored.
    
    * Advance search bug fixed.
    
    * Advance search bug fixed.
    
    * Phase03 refactored to phase09 changes.
    
    * Filter strategies injection improved.
    
    ---------
    
    Co-authored-by: Erfan Rezaei <erfan.rezaei.dev@gmail.com>

---
+++ Phase03/FullTextSearch/InvertedIndex/Dtos/DocumentInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace FullTextSearch.InvertedIndex.Dtos;
+
+public class DocumentInfo
+{
+
+    public required string DocId { get; set; }
+    public required SortedSet<long> Indexes { get; set; }
+}
