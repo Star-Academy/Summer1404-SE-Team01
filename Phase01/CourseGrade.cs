4646ba1eaa4456a5d63c83f300161ca6404a712b
Erfan Rezaei <100679623+ErfanRez@users.noreply.github.com>
2025-08-15 21:08:26 +0330

    Phase06 with CI/CD pipeline created. (#10)
    * Phase06 with CI/CD pipeline created.
    
    * Code base updated
    
    * Base code updated
    
    * README updated.
    
    * Failing test fixed.
    
    * Failing test fixed. Phase01 and Phase02 removed from phase06.

--- Phase01/CourseGrade.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Phase01;
-
-public class CourseGrade
-{
-    public int StudentNumber { get; set; }
-    public string Lesson { get; set; }
-    public double Score { get; set; }
-
-}
---
+++ Phase06/FullTextSearch/InvertedIndex/Dtos/QueryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FullTextSearch.InvertedIndex.Dtos;
+
+public class QueryDto
+{
+
+    public List<string> Required { get; set; } = new();
+    public List<string> Optional { get; set; } = new();
+    public List<string> Excluded { get; set; } = new();
+
+}
