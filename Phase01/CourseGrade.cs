bb5fe5eeb0a4e8ab2eb1f7152387f6cd93823714
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2025-09-03 12:11:03 +0330

    Phase01 and phase02 restored.

---
+++ Phase01/CourseGrade.cs
@@ -0,0 +1,9 @@
+﻿namespace Phase01;
+
+public class CourseGrade
+{
+    public int StudentNumber { get; set; }
+    public string Lesson { get; set; }
+    public double Score { get; set; }
+
+}
---
+++ Phase01/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace Phase01;
+
+public class Student
+{
+    public int StudentNumber { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
